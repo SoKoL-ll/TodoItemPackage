ff6161efbb5b80636d686c1ef373c0918552eb08
SoKoL-ll <hujikolp1907>
2023-07-01 02:41:25 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# TodoItemPackage
+
+A description of this package.
---
+++ Sources/TodoItemPackage/TodoItemPackage.swift
@@ -0,0 +1,6 @@
+public struct TodoItemPackage {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
